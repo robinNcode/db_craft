0dfda90c48479d76bfd1e8c152021b6f7aaeae4b
robinNcode <msmrobin518@gmail.com>
2023-07-21 02:29:37 +0600

    `bug`: Defualt current timestamp syntax.

--- src/Libraries/MigrationGenerator.php
+++ src/Libraries/MigrationGenerator.php
@@ -1,4 +1,6 @@
-<?php namespace Robinncode\DbCraft\Libraries;
+<?php
+
+namespace Robinncode\DbCraft\Libraries;
 
 use CodeIgniter\CLI\CLI;
 use CodeIgniter\Database\BaseConnection;
@@ -73,7 +75,7 @@ class MigrationGenerator
         $tables = $this->db->listTables() ?? [];
 
         if (empty($tables)) {
-            CLI::error('No table found in database');
+            CLI::error('No table found in database!');
             exit(1);
         }
 
@@ -96,7 +98,8 @@ class MigrationGenerator
 
         $relations = $this->generateForeignKeys($table);
 
-        return ['attributes' => $fields,
+        return [
+            'attributes' => $fields,
             'keys' => $indexes . "\n" . $relations
         ];
     }
@@ -127,7 +130,6 @@ class MigrationGenerator
                     if (strlen($item) > 0)
                         $str .= "'$item', ";
                 }
-
             } else {
                 foreach ($arr as $index => $item) {
                     if (strlen($item) > 0)
@@ -140,16 +142,17 @@ class MigrationGenerator
     }
 
     /**
-     * Generate Field array form a table
-     *
+     * Generate Field array from a table
      * @param string $table
-     *
-     * @return string
+     * @return string|null
      */
     protected function generateField(string $table): ?string
     {
         $query = $this->db->query("DESCRIBE $table")->getResult();
         $fieldString = '';
+
+        // Get default values from information_schema
+        $defaultValues = $this->getDefaultValues($table);
 
         foreach ($query as $field) {
             $singleField = "\n\t\t'$field->Field' => [";
@@ -173,17 +176,25 @@ class MigrationGenerator
 
                     if (count($values) == 1)
                         $singleField .= "\n\t\t\t'constraint' => [" . $this->getGluedString($values) . "],";
-
                     else
                         $singleField .= "\n\t\t\t'constraint' => " . $this->getGluedString($values) . ",";
                 }
             }
 
-            //if field need null
+            //if field needs null
             $singleField .= "\n\t\t\t'null' => " . (($field->Null == 'YES') ? 'true,' : 'false,');
 
-            if (!is_null($field->Default) && (strpos($field->Default, 'current_timestamp()') === FALSE))
-                $singleField .= "\n\t\t\t'default' => '$field->Default',";
+            // Fetch and add the default value
+            $default = isset($defaultValues[$field->Field]) ? $defaultValues[$field->Field] : null;
+
+            if (!is_null($default)) {
+                if (strpos($default, 'current_timestamp()') !== false) {
+                    // If the default value is 'current_timestamp()', handle it accordingly.
+                    $singleField .= "\n\t\t\t'default' => \$this->db->query('SELECT current_timestamp() AS time')->getRow()->time,";
+                } else {
+                    $singleField .= "\n\t\t\t'default' => '$default',";
+                }
+            }
 
             //unsigned
             if (strpos($field->Type, 'unsigned') !== false)
@@ -200,9 +211,10 @@ class MigrationGenerator
         return $fieldString;
     }
 
+
     /**
+     * To generate keys from a table ...
      * @param string $table
-     *
      * @return string|null
      */
     protected function generateKeys(string $table): ?string
@@ -216,24 +228,21 @@ class MigrationGenerator
 
         foreach ($index as $key) {
             switch ($key->type) {
-                case 'PRIMARY' :
-                {
-                    $keys['primary'] = "\n\t\t\$this->forge->addPrimaryKey(" .
-                        $this->getGluedString($key->fields) . ");";
-                    break;
-                }
-                case 'UNIQUE' :
-                {
-                    $keys['unique'] .= "\n\t\t\$this->forge->addUniqueKey(" .
-                        $this->getGluedString($key->fields) . ");";
-                    break;
-                }
-                default :
-                {
-                    $keys['foreign'] .= "\n\t\t\$this->forge->addKey(" .
-                        $this->getGluedString($key->fields) . ");";
-                    break;
-                }
+                case 'PRIMARY': {
+                        $keys['primary'] = "\n\t\t\$this->forge->addPrimaryKey(" .
+                            $this->getGluedString($key->fields) . ");";
+                        break;
+                    }
+                case 'UNIQUE': {
+                        $keys['unique'] .= "\n\t\t\$this->forge->addUniqueKey(" .
+                            $this->getGluedString($key->fields) . ");";
+                        break;
+                    }
+                default: {
+                        $keys['foreign'] .= "\n\t\t\$this->forge->addKey(" .
+                            $this->getGluedString($key->fields) . ");";
+                        break;
+                    }
             }
         }
         return implode("\n", $keys);
@@ -252,4 +261,42 @@ class MigrationGenerator
 
         return implode('', array_unique($keyArray));
     }
+
+    /**
+     * Get default values for each column in the table from information_schema
+     * @param string $table
+     * @return array
+     */
+    protected function getDefaultValues(string $table): array
+    {
+        $query = $this->db->query("SELECT DISTINCT COLUMN_NAME, COLUMN_DEFAULT FROM information_schema.COLUMNS WHERE TABLE_NAME = '$table'")->getResultArray();
+
+        $defaultValues = [];
+        foreach ($query as $row) {
+            $columnName = $row['COLUMN_NAME'];
+            $columnDefault = $row['COLUMN_DEFAULT'];
+
+            // Exclude columns with NULL default values
+            if ($columnDefault === 'NULL' || $columnDefault === null) {
+                $defaultValues[$columnName] = null;
+            } elseif (strpos($columnDefault, 'current_timestamp()') !== false) {
+                // Handle current_timestamp() default value
+                $defaultValues[$columnName] = 'current_timestamp()';
+            } else {
+                // Remove quotes from the default value if present
+                $defaultValues[$columnName] = trim($columnDefault, "'");
+            }
+        }
+
+        return $defaultValues;
+    }
+
+    // Ok, we have modify the generateField method. If there is any column has default value current timestamp then the syntax will be:
+    //     "$this->forge->addField("updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP");"
+        
+    //     not this:
+    //     'updated_at' => [
+    //                     'type' => 'TIMESTAMP', // Change the data type according to your column type (e.g., DATETIME, TIMESTAMP, etc.)
+    //                     'default' => 'CURRENT_TIMESTAMP', // Set the current timestamp as the default value
+    //                 ],
 }
