01d8132ce157fcb01d1ab7e355411ae283639eba
robinNcode <msmrobin518@gmail.com>
2024-06-10 05:48:08 +0600

    `fix`: columnName and foreignColumnName fetching

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'robinncode/db_craft',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => '1.2',
+        'version' => '1.2.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'robinncode/db_craft' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => '1.2',
+            'version' => '1.2.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
